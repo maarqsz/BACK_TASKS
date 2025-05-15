8111f0e4abdfd1d076a0c1c5dd91199aa7505690
JOÃO VITOR MADRUGA MARQUES <joao_vm_marques@estudante.sesisenai.org.br>
2025-05-15 20:06:50 -0300

    atualizando  git

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001
 
 // Middleware
 app.use(cors({
-  origin: 'https://back-tasks-m934.onrender.com',
+  origin: 'http://localhost:5173',
   credentials: true,
 }));app.use(express.json())
 app.use(router)
